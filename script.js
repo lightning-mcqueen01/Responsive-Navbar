86acfae6b578e3f7ebfbbbc9d8b5431851f73b82
lightning-mcqueen01 <a2b0h0i2@gmail.com>
2023-09-04 10:31:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+btn.addEventListener('click', function() {
+    links.classList.toggle("show-links")
+    console.log(links.classList);
+})
